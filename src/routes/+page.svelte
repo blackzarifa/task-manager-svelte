1df67262446923a14e32e1cea58d257d1939bdb9
blackzarifa <rafaelirades@gmail.com>
2024-11-06 23:59:17 -0300

    update: btn style

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,7 +4,9 @@
 <div>
 	<section class="mb-8">
 		<div class="flex justify-end">
-			<button>Add Task</button>
+			<button type="button" class="btn btn-md variant-gradient-primary-secondary bg-gradient-to-br"
+				>Add Task</button
+			>
 		</div>
 	</section>
 
