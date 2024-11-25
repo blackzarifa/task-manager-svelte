4cfb71b09a9e18672f21e4c5b2968e758a2f0190
blackzarifa <rafaelirades@gmail.com>
2024-11-25 19:10:53 -0300

    update: basic task structure

--- src/lib/components/tasks/TaskCard.svelte
+++ src/lib/components/tasks/TaskCard.svelte
@@ -4,6 +4,7 @@
 	const { task } = $props<{ task: Task }>();
 </script>
 
-<div class="card variant-filled-primary card-hover p-4">
-	<h4>{task.title}</h4>
+<div class="card variant-filled-primary card-hover my-4 cursor-pointer p-4">
+	<h5 class="h5">{task.title}</h5>
+	<p>{task.description}</p>
 </div>
