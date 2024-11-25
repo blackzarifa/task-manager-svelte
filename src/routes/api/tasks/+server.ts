309e88227d9aaa2fb1524244ab23bdc04ad1ed9f
blackzarifa <rafaelirades@gmail.com>
2024-11-25 16:02:41 -0300

    fix: json

--- src/routes/api/tasks/+server.ts
+++ src/routes/api/tasks/+server.ts
@@ -6,7 +6,7 @@ export const POST: RequestHandler = async ({ request }) => {
 	try {
 		const task: Task = await request.json();
 		// In future: DB operations
-		return json({ task });
+		return json(task);
 	} catch (error) {
 		console.error(error);
 		return new Response(JSON.stringify({ error: 'Failed to create task' }), {
