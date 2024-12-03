4ec3c1686c5db4c3892c71246a0e57c143bcfb8f
blackzarifa <rafaelirades@gmail.com>
2024-12-02 22:02:56 -0300

    feat: dates utils

---
+++ src/lib/utils/dates.ts
@@ -0,0 +1,16 @@
+export function formatDate(dateString: string): string {
+  const date = new Date(dateString);
+  return new Intl.DateTimeFormat('en-US', {
+    month: 'short',
+    day: 'numeric',
+    year: 'numeric',
+  }).format(date);
+}
+
+export function formatDateTime(dateString: string): string {
+  const date = new Date(dateString);
+  return new Intl.DateTimeFormat('en-US', {
+    dateStyle: 'full',
+    timeStyle: 'long',
+  }).format(date);
+}
