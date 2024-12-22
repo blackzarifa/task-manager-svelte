1e90407cc524a687aee283e59b05bb37c51a0b15
blackzarifa <rafaelirades@gmail.com>
2024-12-22 16:16:45 -0300

    refac: id specific endpoint

---
+++ src/routes/api/tasks/[id]/+server.ts
@@ -0,0 +1,14 @@
+import { json } from '@sveltejs/kit';
+import type { RequestHandler } from './$types';
+
+export const DELETE: RequestHandler = async ({ params }) => {
+  try {
+    const taskId = params;
+    console.log(taskId);
+    // DB operations
+    return json({ success: true });
+  } catch (error) {
+    console.error(error);
+    return json({ error: 'Failed to delete tasks' }, { status: 500 });
+  }
+};
