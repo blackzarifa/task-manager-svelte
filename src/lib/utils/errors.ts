ae302618859bbd4682ec0dbba1db65ea905f82cd
blackzarifa <rafaelirades@gmail.com>
2024-12-28 20:49:58 -0300

    add: handleActionError

--- src/lib/utils/errors.ts
+++ src/lib/utils/errors.ts
@@ -1,4 +1,5 @@
 import type { ActionResult } from '@sveltejs/kit';
+import { getModalStore } from '@skeletonlabs/skeleton';
 
 export function parseErrorObject(data: string) {
   const parsed = JSON.parse(data);
@@ -9,4 +10,16 @@ export function getFormActionError(result: ActionResult) {
   if (result.type !== 'failure' || !result.data) return null;
   return parseErrorObject(result.data as unknown as string);
 }
+
+export function handleActionError(data: ActionResult) {
+  if (data.type === 'failure' && data.data) {
+    getModalStore().trigger({
+      type: 'alert',
+      title: 'Error',
+      body: parseErrorObject(data as unknown as string),
+      buttonTextCancel: 'Close',
+    });
+    return true;
+  }
+  return false;
 }
