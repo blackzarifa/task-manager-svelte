fe595257f1f5b73612e25030516a232c440e5b3b
blackzarifa <rafaelirades@gmail.com>
2025-01-08 23:18:06 -0300

    feat: PUT initial logic

--- src/routes/api/tasks/[id]/+server.ts
+++ src/routes/api/tasks/[id]/+server.ts
@@ -12,3 +12,16 @@ export const DELETE: RequestHandler = async ({ params }) => {
     return json({ error: 'Failed to delete task' }, { status: 500 });
   }
 };
+
+export const PUT: RequestHandler = async ({ request, params }) => {
+  try {
+    const taskId = params;
+    const completed: boolean = await request.json();
+    console.log(taskId, completed);
+    // DB operations
+    return json({ success: true });
+  } catch (error) {
+    console.error(error);
+    return json({ error: 'Failed to update task' }, { status: 500 });
+  }
+};
